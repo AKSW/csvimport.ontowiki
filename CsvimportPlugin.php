ee8b92ffeea05655a2b7244e02f3583da5aa83b6
Tim Ermilov <yamalight@gmail.com>
2012-12-03 16:17:09 +0100

    add file

---
+++ CsvimportPlugin.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the {@link http://ontowiki.net OntoWiki} project.
+ *
+ * @copyright Copyright (c) 2012, {@link http://aksw.org AKSW}
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+
+/**
+ * The main class for the csv import plugin.
+ *
+ * @category   OntoWiki
+ * @package    Extensions_Csvimport
+ * @copyright  Copyright (c) 2012 {@link http://aksw.org aksw}
+ * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @author     Tim Ermilov <yamalight@gmail.com>
+ */
+class CsvimportPlugin extends OntoWiki_Plugin
+{
+
+    // ------------------------------------------------------------------------
+    // --- Plugin handler methods ---------------------------------------------
+    // ------------------------------------------------------------------------
+
+    /**
+     * Event handler method, which is called on menu creation. Adds some
+     * datagathering relevant menu entries.
+     *
+     * @param Erfurt_Event $event
+     *
+     * @return bool
+     */
+    public function onCreateMenu($event)
+    {
+        if ($event->isModel) {
+            $url = new OntoWiki_Url(array('controller' => 'csvimport'), array());
+            $menu = $event->menu;
+            $menu->appendEntry('Import CSV Data', (string) $url);
+        }
+
+        return true;
+    }
+}
